034ececfe0dd1a06b9ed176634a02beca1b4bfef
Valter <valteralvees@outlook.com>
2022-04-22 16:59:58 -0300

    Ordenação

---
+++ EmpresaOrdenar/src/empresaordenar/EmpresaOrdenar.java
@@ -0,0 +1,9 @@
+package empresaordenar;
+import java.util.Scanner;
+public class EmpresaOrdenar {
+    public static void main(String[] args) {
+        int alturas[] = {181,121,147,198,134};
+        
+    }
+    
+}
